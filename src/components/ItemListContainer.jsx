0bacc8950cf1d1af4f010fe6f8785f5985826e00
MatiasCortes <matiascortes.dg@gmail.com>
2022-10-20 17:57:56 -0300

    preEntrega1-Cortes

---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,12 @@
+//Creamos el componente ItemListContainer.jsx
+
+const ItemListContainer = ( {greeting} ) => {
+    
+    return (
+        
+        <h1 className="h1-title">Hola! {greeting}</h1>
+
+    ); 
+};
+
+export default ItemListContainer;
