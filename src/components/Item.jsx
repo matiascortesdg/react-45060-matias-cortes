849686078132016f54be6b0f401b4c6c50e62ffb
MatiasCortes <matiascortes.dg@gmail.com>
2022-11-03 17:09:47 -0300

    update files

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -7,7 +7,7 @@ const Item = ( { producto } ) => {
                 <h2 className="h2-title">{producto.title}</h2>
                 <h4 className="descuento">{producto.descuento}% descuento</h4>
                 <h3 className="h3-title">${producto.price}</h3>
-                <h3 className="h3-title">${producto.price * producto.descuento/100}</h3>
+                <h3 className="h3-title">${(producto.price * producto.descuento)/100}</h3>
 
 
             </arcticle>
